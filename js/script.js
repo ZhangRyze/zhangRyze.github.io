d094619840ffd99266a1928fb06269f873af682e
张然 <ryze15620920109@163.com>
2018-03-13 19:40:46 +0800

    Site updated: 2018-03-13 19:40:46

--- js/script.js
+++ js/script.js
@@ -1,12 +1,93 @@
-(function(){
-    var valine = new Valine({
-        el:'#vcomments',
-        notify:#{theme.valine.notify} || false,
-        verify:#{theme.valine.verify}|| false,
-        app_id:'#{theme.valine.appid}',
-        app_key:'#{theme.valine.appkey}',
-        placeholder:'#{theme.valine.placeholder}',
-        path: window.location.pathname,
-        avatar:'#{theme.valine.avatar}'
-    })
-})()
+function openSearch() {
+	$("#modal-container-bg").show();
+	$("#local-search-box").show();
+}
+
+function closeSearch() {
+	$("#modal-container-bg").hide();
+	$("#local-search-box").hide();
+}
+// 请求点击数，显示在页面上
+function showHitCount(Counter) {
+	// 这是给一个页面中有多篇文章时所调用的，例如博客主界面或者存档界面。
+	var query = new AV.Query(Counter);
+	var entries = [];
+	var _visitors = $(".post-hits"); // 获取页面中所有文章的阅读数容器
+
+	_visitors.each(function() {
+		entries.push($(this).attr("id").trim());
+	});
+
+	// 批量查询
+	query.containedIn('url', entries);
+	query.find({
+		success:function(results){
+			if (results.length === 0) {
+				_visitors.html('阅读数: 0');
+				return;
+			}
+
+			for (var i = 0; i < results.length; i++) {
+				var item = results[i];
+				var url = item.get('url');
+				var hits = item.get('hits'); // 获取点击次数
+				$('#' + url).html('阅读数：' + hits + '次')
+			}
+		},
+		error: function(object, error) {
+			console.log("Error: " + error.code + " " + error.message);
+		}
+	})
+}
+// 为页面添加点击数
+function addCount(Counter) {
+	// 页面（博客文章）中的信息：leancloud_visitors
+	// id为page.url， data-flag-title为page.title
+	var _visitors = $(".post-hits");
+	var url = _visitors.attr('id').trim();
+	var title = _visitors.data('post-title').trim();
+	var query = new AV.Query(Counter);
+
+	// 只根据文章的url查询LeanCloud服务器中的数据
+	query.equalTo("url", url);
+	query.find({
+		success: function(results) {
+			if (results.length > 0) { //说明LeanCloud中已经记录了这篇文章
+				var counter = results[0];
+				counter.fetchWhenSave(true);
+				counter.increment("hits"); // 将点击次数加1
+				counter.save(null, {
+					success: function(counter) {
+						$('#' + url).html('阅读数' + counter.get('hits') + '次')
+					},
+					error: function(counter, error) {
+						console.log('Failed to save Visitor num, with error message: ' + error.message);
+					}
+				});
+			} else {
+				// 执行这里，说明LeanCloud中还没有记录此文章
+				var newcounter = new Counter();
+				/* Set ACL */
+				var acl = new AV.ACL();
+				acl.setPublicReadAccess(true);
+				acl.setPublicWriteAccess(true);
+				newcounter.setACL(acl);
+				/* End Set ACL */
+				newcounter.set("title", title); // 把文章标题
+				newcounter.set("url", url); // 文章url
+				newcounter.set("hits", 1); // 初始点击次数：1次
+				newcounter.save(null, { // 上传到LeanCloud服务器中
+					success: function(newcounter) {
+						$('#' + url).html('阅读数' + newcounter.get('hits') + '次')
+					},
+					error: function(newcounter, error) {
+						console.log('Failed to create');
+					}
+				});
+			}
+		},
+		error: function(error) {
+			console.log('Error:' + error.code + " " + error.message);
+		}
+	});
+}
---
+++ js/search.my.js
@@ -0,0 +1,77 @@
+var searchFunc = function(path, search_id, content_id) {
+    'use strict';
+    $.ajax({
+        url: path,
+        dataType: "json",
+        success: function( results ) {
+            // get the contents from search data
+            var _input = document.getElementById(search_id);
+            if (!_input) return;
+            var _resultContent = document.getElementById(content_id);
+
+            _input.addEventListener('input', function(){
+                var str='<ul class=\"search-result-list\">';
+                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+                _resultContent.innerHTML = "";
+                if (this.value.trim().length <= 0) {
+                    _resultContent.innerHTML = "<div class='local-search-prompt'>搜一搜看看吧 ~</div>";
+                    return;
+                }
+                // perform local searching
+                results.forEach(function(data) {
+                    var isMatch = true;
+                    var content_index = [];
+                    if (!data.title || data.title.trim() === '') {
+                        data.title = "Untitled";
+                    }
+                    var data_title = data.title.trim().toLowerCase();
+                    var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
+                    var data_url = data.url;
+                    var data_url_decode = decodeURIComponent(data.url).split('/');
+                    var data_date = null;
+                    if(data_url_decode.length == 6){
+                        data_date = data_url_decode[1] + '-' + data_url_decode[2] + '-' + data_url_decode[3];
+                    }
+                    console.log(data_date)
+                    var index_title = -1;
+                    var index_content = -1;
+                    var first_occur = -1;
+                    // only match artiles with not empty contents
+                    if (data_content !== '') {
+                        keywords.forEach(function(keyword, i) {
+                            index_title = data_title.indexOf(keyword);
+                            index_content = data_content.indexOf(keyword);
+                            if( index_title < 0 && index_content < 0 ){
+                                isMatch = false;
+                            } else {
+                                if (index_content < 0) {
+                                    index_content = 0;
+                                }
+                                if (i == 0) {
+                                    first_occur = index_content;
+                                }
+                                // content_index.push({index_content:index_content, keyword_len:keyword_len});
+                            }
+                        });
+                    } else {
+                        isMatch = false;
+                    }
+                    // show search results
+                    if (isMatch) {
+                        str += "<li>";
+                        str += "<a href='"+ data_url +"'>";
+                        str += "<div class='search-result-title'><div>"+ data_title +"</div>";
+                        str += "<div><div><i class='iconfont ic-category'></i>"+ data.categories[0] +"</div><div><i class='iconfont ic-tag'></i>"+ data.tags[0] +"</div></div>";
+                        str += "</div><div>"+ data_date +"</div>";
+                        str += "</a></li>";
+                    }
+                });
+                str += "</ul>";
+                _resultContent.innerHTML = str;
+            });
+        }
+    });
+}
+// search
+var path = '/search.json';
+searchFunc(path, 'local-search-input', 'local-search-result')
